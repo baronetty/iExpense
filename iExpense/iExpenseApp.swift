fa65e5d8f1d0e1b748f18f47f63484a521604f89
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-20 12:51:45 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Leo  on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
