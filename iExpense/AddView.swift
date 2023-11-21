ca586028e73e921115899e3ccc1cd161e7f2bfb7
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-21 22:05:27 +0200

    Version 1.0

---
+++ iExpense/AddView.swift
@@ -0,0 +1,49 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Leo  on 21.11.23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
