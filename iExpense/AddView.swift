0027551b2abafe8e104c357c8e9821a4d8ffceff
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-30 23:25:29 +0200

    final Version

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct AddView: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var name = ""
+//    @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
+    @State private var title = "Name"
     
     var expenses: Expenses
     
@@ -21,7 +22,7 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
+//                TextField("Name", text: $name)
                 
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) {
@@ -29,17 +30,19 @@ struct AddView: View {
                     }
                 }
                 
-                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add New Expense")
             .toolbar {
                 Button("Save") {
-                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    let item = ExpenseItem(name: title, type: type, amount: amount)
                     expenses.items.append(item)
                     dismiss()
                 }
             }
+            .navigationTitle($title)
+            .navigationBarTitleDisplayMode(.inline)
+            .disabled(title == "Name")
         }
     }
 }
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -36,41 +36,74 @@ class Expenses {
     }
 }
 
+
+
+struct Style: ViewModifier {
+    let amount: Double
+    
+    func body(content: Content) -> some View {
+        content.foregroundColor(amountColor)
+    }
+    
+    private var amountColor: Color {
+        switch amount {
+        case ..<10.0:
+            return .green
+        case ..<100.0:
+            return .blue
+        default:
+            return .red
+        }
+    }
+}
+
+extension View {
+    func amountStyle(amount: Double) -> some View {
+        self.modifier(Style(amount: amount))
+    }
+}
+
+
+
 struct ContentView: View {
     @State private var expenses = Expenses()
     @State private var showingAddExpense = false
+    
+    
+    
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach(expenses.items) { item in // need that for delete stuff later on
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                
+                                Text(item.type)
+                            }
                             
-                            Text(item.type)
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .amountStyle(amount: item.amount)
                         }
-                        
-                        Spacer()
-                        
-                        Text(item.amount, format: .currency(code: "USD£"))
-                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("iExpense")
             .toolbar {
-                Button ("Add Expense", systemImage: "plus") {
-                    showingAddExpense = true
+                NavigationLink {
+                    AddView(expenses: expenses)
+                } label: {
+                    Label("Add Expense", systemImage: "plus")
                 }
-            }
-            .sheet(isPresented: $showingAddExpense) {
-                AddView(expenses: expenses)
             }
         }
     }
-
+    
     func removeItems(at offsets: IndexSet) {
         expenses.items.remove(atOffsets: offsets)
     }
